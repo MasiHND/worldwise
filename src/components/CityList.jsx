48bf1a7cfe1b4a07cef77263f98cf1453d16a20e
MasiHND <pouya.hendy@gmail.com>
2025-02-14 12:43:03 +0330

    updated app cities component

--- src/components/CityList.jsx
+++ src/components/CityList.jsx
@@ -1,10 +1,12 @@
 import CityItem from "./CityItem";
 import styles from "./CityList.module.css";
 import Spinner from "./Spinner";
+import Message from "./Message";
 
 function CityList({ cities, isLoading }) {
   if (isLoading) return <Spinner />;
 
+  if (!cities.length) return <Message message="Add Your First City By Clicking On The Map 🥰"/>;
   return (
     <ul className={styles.cityList}>
       {cities.map((city) => (
