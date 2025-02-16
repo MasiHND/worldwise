b8fbc79d5a7108dafc9c9f1e26079edd52f2f74d
MasiHND <pouya.hendy@gmail.com>
2025-02-16 13:51:23 +0330

    CitiesContext aded

---
+++ src/contexts/CitiesContext.jsx
@@ -0,0 +1,43 @@
+import { useState, useEffect, createContext, useContext } from "react";
+
+const Base_URL = "http://localhost:5000";
+
+const CitiesContext = createContext();
+
+function CitiesProvider({ children }) {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${Base_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+        setIsLoading(false);
+      } catch {
+        alert("An error occurred. Please try again later.");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
+  return (
+    <CitiesContext.Provider value={{ cities, isLoading }}>
+      {children}
+    </CitiesContext.Provider>
+  );
+}
+
+function useCities() {
+  const context = useContext(CitiesContext);
+  if (!context) {
+    throw new Error("useCities must be used within a CitiesProvider");
+  }
+  return context;
+}
+
+export { CitiesProvider, useCities };
